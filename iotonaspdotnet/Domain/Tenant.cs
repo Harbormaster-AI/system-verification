bb6a6715841f6e279a267b09475d6a0a86b27756
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-18 15:44:43 -0400

    Verification of a system generation on ASP.NET-10-xcAVyalzbb

---
+++ iotonaspdotnet/Domain/Tenant.cs
@@ -0,0 +1,32 @@
+using iotonaspdotnet.Contracts;
+
+namespace iotonaspdotnet.Domain;
+
+public class Tenant
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+ public virtual long? TenantId { get; set; } 
+ public virtual string? Name { get; set; } 
+public virtual Site? Sites { get; set; } 
+public virtual TenantUser? Users { get; set; } 
+public virtual IoTDevice? Devices { get; set; } 
+public virtual DataRetentionPolicy? DataRetentionPolicies { get; set; } 
+public virtual ConnectivityPlan? ConnectivityPlans { get; set; } 
+public virtual SimCard? SimCards { get; set; } 
+public virtual MessagingEndpoint? MessagingEndpoints { get; set; } 
+public virtual AccessPolicy? AccessPolicies { get; set; } 
+public virtual DeviceGroup? DeviceGroups { get; set; } 
+public virtual AlertRule? AlertRules { get; set; } 
+public virtual MaintenanceTicket? MaintenanceTickets { get; set; } 
+public virtual UsageRecord? UsageRecords { get; set; } 
+ public virtual TenantType? TenantType { get; set; } 
+
+    public static Tenant FromRequest(TenantRequest request) {
+        return new Tenant {
+            Id = request.Id,
+            Name = request.Name,
+            TenantType = request.TenantType,
+        };
+    }
+}
---
+++ iotonaspdotnet/Domain/TenantUser.cs
@@ -0,0 +1,26 @@
+using iotonaspdotnet.Contracts;
+
+namespace iotonaspdotnet.Domain;
+
+public class TenantUser
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+ public virtual long? TenantuserId { get; set; } 
+ public virtual string? FirstName { get; set; } 
+ public virtual string? LastName { get; set; } 
+ public virtual string? Email { get; set; } 
+public virtual Tenant? Tenant { get; set; } 
+public virtual CommandInvocation? CommandInvocations { get; set; } 
+ public virtual UserRole? Role { get; set; } 
+
+    public static TenantUser FromRequest(TenantUserRequest request) {
+        return new TenantUser {
+            Id = request.Id,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            Role = request.Role,
+        };
+    }
+}
