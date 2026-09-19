623d19180d48bfb2441d758614d8f03788cde070
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-19 12:52:23 -0400

    Verification of a system generation on ASP.NET-10-AWn3TRReAJ

---
+++ iotonaspdotnet/Domain/NetworkProfile.cs
@@ -0,0 +1,27 @@
+using iotonaspdotnet.Contracts;
+
+namespace iotonaspdotnet.Domain;
+
+public class NetworkProfile
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+ public virtual long? NetworkprofileId { get; set; } 
+ public virtual string? ProfileName { get; set; } 
+ public virtual string? Ssid { get; set; } 
+ public virtual string? Apn { get; set; } 
+public virtual IoTDevice? Device { get; set; } 
+public virtual Gateway? Gateway { get; set; } 
+public virtual SimCard? SimCard { get; set; } 
+ public virtual ConnectivityType? ConnectivityType { get; set; } 
+
+    public static NetworkProfile FromRequest(NetworkProfileRequest request) {
+        return new NetworkProfile {
+            Id = request.Id,
+            ProfileName = request.ProfileName,
+            Ssid = request.Ssid,
+            Apn = request.Apn,
+            ConnectivityType = request.ConnectivityType,
+        };
+    }
+}
