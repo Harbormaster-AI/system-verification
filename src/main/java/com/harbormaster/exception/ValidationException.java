3a0bc5306e24e8c02cb7de93120a8a6512e20c02
HarborMaster Verification <dev@harbormaster.net>
2026-09-22 23:48:10 +0000

    chore: format generated Spring Boot source

--- src/main/java/com/harbormaster/exception/ValidationException.java
+++ src/main/java/com/harbormaster/exception/ValidationException.java
@@ -2,11 +2,11 @@ package com.harbormaster.exception;
 
 public class ValidationException extends BusinessException {
 
-    public ValidationException(String entity, String operation ) {
-        super(  "VALIDATION_ERROR",
-                entity,
-                operation,
-                "Failed during validation of " + entity + " signaled during operation " + operation
-                );
-    }
+  public ValidationException(String entity, String operation) {
+    super(
+        "VALIDATION_ERROR",
+        entity,
+        operation,
+        "Failed during validation of " + entity + " signaled during operation " + operation);
+  }
 }
