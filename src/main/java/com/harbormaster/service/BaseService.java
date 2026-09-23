d22fe9094a2d958a1715098f9cc7431bd88b836c
HarborMaster Verification <dev@harbormaster.net>
2026-09-23 00:34:41 +0000

    chore: format generated Spring Boot source

--- src/main/java/com/harbormaster/service/BaseService.java
+++ src/main/java/com/harbormaster/service/BaseService.java
@@ -23,34 +23,30 @@
  */
 package com.harbormaster.service;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import org.springframework.beans.BeansException;
-
 /**
  * Base class for application business service.
+ *
  * <p>
+ *
  * @author Harbormaster Dev Team
  */
 @Component
 public class BaseService implements ApplicationContextAware {
- 
-	public BaseService() {	
-	}
-	
-	@Override
-    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
-        applicationContext = ctx;
-    }    
 
-//************************************************************************
-// Protected / Private Methods
-//************************************************************************
-    protected static ApplicationContext applicationContext;
+  public BaseService() {}
 
+  @Override
+  public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+    applicationContext = ctx;
+  }
+
+  // ************************************************************************
+  // Protected / Private Methods
+  // ************************************************************************
+  protected static ApplicationContext applicationContext;
 }
-
-
-
