4e1e8b996f5f5ed01e3bf929546276e159740507
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-25 11:39:52 -0400

    Verification of a system generation on ASP.NET-10-GBaIKDyBvx

---
+++ bankingonaspdotnet/Service/LoanPaymentService.cs
@@ -0,0 +1,233 @@
+
+using bankingonaspdotnet.Domain;
+using bankingonaspdotnet.Persistence;
+using bankingonaspdotnet.Contracts;
+using bankingonaspdotnet.Telemetry;
+
+namespace bankingonaspdotnet.Service;
+
+public interface ILoanPaymentService {
+
+    Task Create(LoanPayment model , CancellationToken cancellationToken);
+    Task<bool> Update(LoanPayment model, CancellationToken cancellationToken);
+    Task<LoanPayment?> Get(IdentifierRequest identifier, CancellationToken cancellationToken);
+    Task<IReadOnlyList<LoanPayment>> GetAll(CancellationToken cancellationToken);
+    Task<bool> Delete(IdentifierRequest identifier, CancellationToken cancellationToken);
+    // ------------------------------
+    // Single Associations
+    // -------------------------------
+    Task<bool> AssignLoanAccount(AssociationRequest request, CancellationToken cancellationToken);
+    Task<bool> UnassignLoanAccount(AssociationRequest request, CancellationToken cancellationToken);
+    Task<bool> AssignTransaction(AssociationRequest request, CancellationToken cancellationToken);
+    Task<bool> UnassignTransaction(AssociationRequest request, CancellationToken cancellationToken);
+
+
+}
+
+public class LoanPaymentService : ILoanPaymentService
+{
+    private readonly ApplicationTelemetry _telemetry;
+    private readonly ILoanPaymentRepository _repository;
+    private readonly ILogger<LoanPaymentService> _logger;
+    private readonly IServiceResolver _serviceResolver;
+
+
+    public LoanPaymentService(
+        ApplicationTelemetry telemetry,
+        ILoanPaymentRepository repository,
+        ILogger<LoanPaymentService> logger,
+        IServiceResolver serviceResolver)
+    {
+        _telemetry = telemetry;
+        _repository = repository;
+        _logger = logger;
+        _serviceResolver = serviceResolver;
+    }
+
+    public async Task Create(LoanPayment model, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _telemetry.Execute(
+                "LoanPayment",
+                "CreateLoanPayment",
+                () => _repository.AddAsync(model, cancellationToken));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                    ex,
+                    "Unexpected error while creating Transaction.");
+        }
+    }
+
+    public async Task<bool> Update(LoanPayment model, CancellationToken cancellationToken)
+    {
+        try {
+            var existing = await _repository.GetByIdAsync(model.Id, cancellationToken);
+            if (existing is null)
+            {
+                return false;
+            }
+            existing.PaymentReference = model.PaymentReference;
+            existing.Amount = model.Amount;
+            existing.PaymentDate = model.PaymentDate;
+            existing.Method = model.Method;
+            existing.Status = model.Status;
+
+            await _telemetry.Execute(
+                "LoanPayment",
+                "UpdateLoanPayment",
+                () => _repository.UpdateAsync(existing, cancellationToken));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                    ex,
+                    "Unexpected error while creating Transaction.");
+            return false;
+        }
+        return true;
+    }
+
+    public Task<LoanPayment?> Get(IdentifierRequest identifier, CancellationToken cancellationToken)
+    => _repository.GetByIdAsync(identifier.Id, cancellationToken);
+
+    public Task<IReadOnlyList<LoanPayment>> GetAll(CancellationToken cancellationToken)
+    => _repository.GetAllAsync(cancellationToken);
+
+    public async Task<bool> Delete(IdentifierRequest identifier, CancellationToken cancellationToken)
+    {
+        var existing = await _repository.GetByIdAsync(identifier.Id, cancellationToken);
+        if (existing is null)
+        {
+            return false;
+        }
+
+        try
+        {
+            await _telemetry.Execute(
+                "LoanPayment",
+                "UpdateLoanPayment",
+                () => _repository.DeleteAsync(existing, cancellationToken));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                    ex,
+                    "Unexpected error while creating Transaction.");
+            return false;
+        }
+        return true;
+    }
+
+    public async Task<bool> AssignLoanAccount(AssociationRequest request, CancellationToken cancellationToken) {
+
+        var parent = await _repository.GetByIdAsync(request.ParentId, cancellationToken);
+        if (parent is null)
+        {
+            _logger.LogError("No LoanPayment found using Id {ParentId}", request.ParentId);
+            return false;
+        }
+
+        try
+        {
+            var childRequest = new IdentifierRequest
+            {
+                Id = request.ChildId,
+            };
+
+            var child = await _serviceResolver.Get<LoanAccountService>().Get(childRequest, cancellationToken);
+            parent.LoanAccount = child;
+            await Update( parent, cancellationToken );
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                    ex,
+                    "Unexpected error while creating Transaction.");
+            return false;
+        }
+        return true;
+    }
+
+    public async Task<bool> UnassignLoanAccount(AssociationRequest request, CancellationToken cancellationToken) {
+        var parent = await _repository.GetByIdAsync(request.ParentId, cancellationToken);
+        if (parent is null)
+        {
+            _logger.LogError("No LoanPayment found using Id {ParentId}", request.ParentId);
+            return false;
+        }
+
+        try
+        {
+            parent.LoanAccount = null;
+            await Update( parent, cancellationToken );
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                    ex,
+                    "Unexpected error while creating Transaction.");
+            return false;
+        }
+        return true;
+    }
+
+    public async Task<bool> AssignTransaction(AssociationRequest request, CancellationToken cancellationToken) {
+
+        var parent = await _repository.GetByIdAsync(request.ParentId, cancellationToken);
+        if (parent is null)
+        {
+            _logger.LogError("No LoanPayment found using Id {ParentId}", request.ParentId);
+            return false;
+        }
+
+        try
+        {
+            var childRequest = new IdentifierRequest
+            {
+                Id = request.ChildId,
+            };
+
+            var child = await _serviceResolver.Get<TransactionService>().Get(childRequest, cancellationToken);
+            parent.Transaction = child;
+            await Update( parent, cancellationToken );
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                    ex,
+                    "Unexpected error while creating Transaction.");
+            return false;
+        }
+        return true;
+    }
+
+    public async Task<bool> UnassignTransaction(AssociationRequest request, CancellationToken cancellationToken) {
+        var parent = await _repository.GetByIdAsync(request.ParentId, cancellationToken);
+        if (parent is null)
+        {
+            _logger.LogError("No LoanPayment found using Id {ParentId}", request.ParentId);
+            return false;
+        }
+
+        try
+        {
+            parent.Transaction = null;
+            await Update( parent, cancellationToken );
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                    ex,
+                    "Unexpected error while creating Transaction.");
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+}
