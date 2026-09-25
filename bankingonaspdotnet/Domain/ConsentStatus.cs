8dd4a72d0b7b333a2fe18bd0d6b4628e3699d208
HarborMaster Verification <dev@harbormaster.net>
2026-09-25 18:08:40 +0000

    chore: format generated ASP.NET source

--- bankingonaspdotnet/Domain/ConsentStatus.cs
+++ bankingonaspdotnet/Domain/ConsentStatus.cs
@@ -36,7 +36,7 @@ namespace bankingonaspdotnet.Domain;
 
 public enum ConsentStatus
 {
-    Active=1,Revoked=2,Expired=3
+    Active = 1, Revoked = 2, Expired = 3
 }
 
 /// <summary>
--- bankingonaspdotnet/Domain/Customer.cs
+++ bankingonaspdotnet/Domain/Customer.cs
@@ -7,30 +7,32 @@ public class Customer
 {
     public Guid Id { get; set; } = Guid.NewGuid();
 
- public virtual long? CustomerId { get; set; } 
- public virtual string? FirstName { get; set; } 
- public virtual string? LastName { get; set; } 
- public virtual string? LegalName { get; set; } 
- public virtual DateOnly? DateOfBirth { get; set; } 
- public virtual string? TaxId { get; set; } 
- public virtual string? Email { get; set; } 
- public virtual string? Phone { get; set; } 
- public virtual Address? Address { get; set; } 
-public virtual Bank? Bank { get; set; } 
-public virtual ICollection<Account> Accounts { get; set; } = new List<Account>();
-public virtual ICollection<LoanAccount> LoanAccounts { get; set; } = new List<LoanAccount>();
-public virtual ICollection<PaymentCard> PaymentCards { get; set; } = new List<PaymentCard>();
-public virtual ICollection<ExternalAccount> ExternalAccounts { get; set; } = new List<ExternalAccount>();
-public virtual ICollection<FundsTransfer> FundsTransfers { get; set; } = new List<FundsTransfer>();
-public virtual ICollection<Dispute> Disputes { get; set; } = new List<Dispute>();
-public virtual ICollection<KycProfile> KycProfiles { get; set; } = new List<KycProfile>();
-public virtual ICollection<Consent> Consents { get; set; } = new List<Consent>();
- public virtual CustomerType? CustomerType { get; set; } 
- public virtual RiskRating? RiskRating { get; set; } 
- public virtual KycStatus? KycStatus { get; set; } 
+    public virtual long? CustomerId { get; set; }
+    public virtual string? FirstName { get; set; }
+    public virtual string? LastName { get; set; }
+    public virtual string? LegalName { get; set; }
+    public virtual DateOnly? DateOfBirth { get; set; }
+    public virtual string? TaxId { get; set; }
+    public virtual string? Email { get; set; }
+    public virtual string? Phone { get; set; }
+    public virtual Address? Address { get; set; }
+    public virtual Bank? Bank { get; set; }
+    public virtual ICollection<Account> Accounts { get; set; } = new List<Account>();
+    public virtual ICollection<LoanAccount> LoanAccounts { get; set; } = new List<LoanAccount>();
+    public virtual ICollection<PaymentCard> PaymentCards { get; set; } = new List<PaymentCard>();
+    public virtual ICollection<ExternalAccount> ExternalAccounts { get; set; } = new List<ExternalAccount>();
+    public virtual ICollection<FundsTransfer> FundsTransfers { get; set; } = new List<FundsTransfer>();
+    public virtual ICollection<Dispute> Disputes { get; set; } = new List<Dispute>();
+    public virtual ICollection<KycProfile> KycProfiles { get; set; } = new List<KycProfile>();
+    public virtual ICollection<Consent> Consents { get; set; } = new List<Consent>();
+    public virtual CustomerType? CustomerType { get; set; }
+    public virtual RiskRating? RiskRating { get; set; }
+    public virtual KycStatus? KycStatus { get; set; }
 
-    public static Customer FromRequest(CustomerRequest request) {
-        return new Customer {
+    public static Customer FromRequest(CustomerRequest request)
+    {
+        return new Customer
+        {
             Id = request.Id,
             FirstName = request.FirstName,
             LastName = request.LastName,
